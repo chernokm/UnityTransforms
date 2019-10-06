b1f883dd57f1905fcee9b90b5c934698f9a23903
chernokm <mac_chernok@yahoo.com>
2019-10-06 11:13:11 -0400

    Rotation, Translate, and Scale

---
+++ Transforms3D/Assets/Scripts/Scale.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    public float speed = 5f;
+    Vector3 temp;
+    Vector3 temp2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        temp = transform.localScale;
+        temp.x += Time.deltaTime;
+        transform.localScale = temp;
+
+        temp2 = transform.localScale;
+        temp2.z += Time.deltaTime;
+        transform.localScale = temp2;
+    }
+}
---
+++ Transforms3D/Assets/Scripts/Translate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate : MonoBehaviour
+{
+    float speedX = 1;
+    float speedY = 0;
+    float speedZ = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(speedX, speedY, speedZ) * Time.deltaTime);
+    }
+}
